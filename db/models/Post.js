8ab491ea01a35677226e862998420d03b38a4fd0
sebapinery <sebapinery@gmail.com>
2021-05-20 00:48:50 -0300

    Model validation added

--- db/models/Post.js
+++ db/models/Post.js
@@ -11,8 +11,20 @@ Post.init(
         notNull: { msg: "Title must not be empty" },
       },
     },
-    content: DataTypes.TEXT,
-    image: DataTypes.STRING,
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Content must not be empty" },
+      },
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Image must not be empty" },
+      },
+    },
   },
   {
     sequelize,
